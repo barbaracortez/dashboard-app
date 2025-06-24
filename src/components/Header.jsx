1daae5834bddeec92b3ba0781d1220c8714f9733
barbaracortez <cortezbarbara.dev@gmail.com>
2025-06-24 16:49:50 -0300

    menu al avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
-
+import { useState, useRef, useEffect } from 'react';
 import "../styles/App.css";
+import avatar from '../assets/avatar-usuario.png';
 
 function Header() {
+    const [ menuAbierto, setMenuAbierto] = useState(false);
+    const avatarRef = useRef(null);
+
+
+    const toggleMenu = () => {
+        setMenuAbierto(!menuAbierto);
+    }
+
+    useEffect(()=> {
+        function handleClickOutside(event) {
+            if (avatarRef.current && !avatarRef.current.contains(event.target)){
+                setMenuAbierto(false);
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        };
+    }, []);
+
   return (
     <header className="header">
       <div className="header-left">
@@ -9,6 +31,21 @@ function Header() {
       </div>
       <div className="header-right">
         <input type="text" placeholder="Buscar..." className="search-input" />
+
+        <div className="avatar-container" onClick={toggleMenu} ref={avatarRef}>
+
+          <img src={avatar} alt="Usuario" className="avatar" />
+          
+          {menuAbierto && (
+            <div className="user-menu">
+              <ul>
+                <li>Mi cuenta</li>
+                <li>Configuraciones</li>
+                <li>Cerrar Sesión</li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
